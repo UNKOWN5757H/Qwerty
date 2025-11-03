ddff09c1616d12aafee85e3829c6eee40a8a00cb
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2025-11-03 21:54:23 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -17,7 +17,7 @@ async def settings(client, query):
     total_db_channels = len(getattr(client, 'db_channels', {}))
     primary_db = getattr(client, 'primary_db_channel', client.db)
     
-    msg = f"""<blockquote>✦ sᴇᴛᴛɪɴɢs ᴏғ @{client.username}</blockquote>
+    msg = f"""<blockquote>✦ sᴇᴛᴛɪɴɢs ᴏғ @{client.me.username}</blockquote>
 ›› **ꜰꜱᴜʙ ᴄʜᴀɴɴᴇʟs:** `{total_fsub}` (ʀᴇǫᴜᴇsᴛ: {request_enabled}, ᴛɪᴍᴇʀ: {timer_enabled})
 ›› **ᴅʙ ᴄʜᴀɴɴᴇʟs:** `{total_db_channels}` (ᴘʀɪᴍᴀʀʏ: `{primary_db}`)
 ›› **ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇʀ:** `{client.auto_del}`
